971ff16e3ceb07e3c7b2729a7ada084e6d1dfb59
x-255 <ouhuangff@163.com>
2023-07-09 14:43:38 +0800

    优化倒计时

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 
-IS_DEBUG = False
+IS_DEBUG = True
 LOGIN_URL = 'https://login.taobao.com'
 COOKIE_FILE = 'cookies.pkl'
 COOKIE_EXPIRED_FILE = 'cookie_expired_time.pkl'
 
 config = {
     'targetUrl': 'https://cart.taobao.com/cart.htm?from=btop', # 购物车地址
-    'targetTime': '2023-07-05 20:00:00', # 抢购时间
+    'targetTime': '2023-07-09 14:18:00', # 抢购时间
     'maxRetry': 3, # 没抢到时的最大重试次数
     'leadTime': 150, # 提前多少毫秒开始抢购
 }
+
+def log(msg):
+    print(f'[{datetime.now()}] {msg}')
 
 
 def create_webdriver():
@@ -74,11 +77,11 @@ def login():
         login_by_cookies()
     else:
         login_by_manual()
-    print('登录成功...')
+    log('登录成功...')
 
 
 def login_by_manual():
-    print('请手动登录...')
+    log('请手动登录...')
     while True:
         if not LOGIN_URL in wd.current_url:
             break
@@ -88,7 +91,7 @@ def login_by_manual():
 
 
 def login_by_cookies():
-    print('正在使用缓存登录...')
+    log('正在使用缓存登录...')
     for cookie in pickle.load(open(COOKIE_FILE, 'rb')):
         wd.add_cookie({
             'domain': cookie['domain'],
@@ -106,7 +109,7 @@ def check_all_goods():
     try:
         check_all = wd.find_element(By.XPATH, '//*[@id="J_SelectAll1"]/div/label')
     except NoSuchElementException:
-        print('购物车空空如也，请添加商品后重新运行脚本...')
+        log('购物车空空如也，请添加商品后重新运行脚本...')
         wd.quit()
         return
     else:
@@ -116,29 +119,34 @@ def check_all_goods():
 def buy(retry = config['maxRetry']):
     wd.find_element(By.XPATH, '//a[@class="submit-btn"]').click()
 
+    
+    
+    sub_btn = wd.find_element(By.CSS_SELECTOR, '.go-btn')
+    log('提交订单...')
     if IS_DEBUG:
         return
     
-    wd.find_element(By.CSS_SELECTOR, '.go-btn').click()
+    sub_btn.click()
 
     if '支付宝' in wd.title:
-        print('抢购成功...')
+        log('抢购成功...')
     else:
-        print('抢购失败...')
+        log('抢购失败...')
 
 
 def scheduler():
-    now = get_taobao_time()
-    target_time = datetime.strptime(config['targetTime'], '%Y-%m-%d %H:%M:%S')
+    taobao_timediff = get_taobao_timediff()
+    target_time = datetime.strptime(config['targetTime'], '%Y-%m-%d %H:%M:%S') - taobao_timediff
     
+    now = datetime.now()
     if now >= target_time:
-        print('开始抢购...')
+        log('开始抢购...')
         check_all_goods()
         buy()
     else:
         wake_up_time = 60 * 10
         diff = (target_time - now).total_seconds()
-        print(f'距离抢购时间还有 {diff} 秒')
+        log(f'距离抢购时间还有 {diff} 秒')
         
         if diff > wake_up_time:
             time.sleep(wake_up_time)
@@ -148,24 +156,25 @@ def scheduler():
             return
         
         check_all_goods()
-        print('等待抢购...')
-        time.sleep(diff)
-        print('开始抢购...')
+        log('等待抢购...')
+        while True:
+            if datetime.now() >= target_time:
+                break
+        log('开始抢购...')
         buy()
         set_cookie_expired_time()
 
 
 
-def get_taobao_time():
+def get_taobao_timediff():
     taobao_time_url = 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
     }
-    start_time = datetime.now()
+    local_time = datetime.now()
     taobao_time_stamp = requests.get(url=taobao_time_url, headers=headers).json()['data']['t']
-    req_time = (datetime.now() - start_time).total_seconds()
     
-    return datetime.fromtimestamp(int(taobao_time_stamp) / 1000 + req_time)
+    return local_time - datetime.fromtimestamp(int(taobao_time_stamp) / 1000)
 
 
 if __name__ == '__main__':
